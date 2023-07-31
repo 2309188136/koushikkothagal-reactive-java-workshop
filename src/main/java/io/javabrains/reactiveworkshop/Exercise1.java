0032065a30b64e560e46d1012ce3ac30a4c6d7a6
2309188136 <2309188136@qq.com>
2023-07-31 12:28:06 +0800

    begin exercise 5

--- src/main/java/io/javabrains/reactiveworkshop/Exercise1.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise1.java
@@ -14,16 +14,28 @@ public class Exercise1 {
 
         // Print the second and third numbers in intNumbersStream that's greater than 5
         // TODO: Write code here
+        StreamSources.intNumbersStream().filter(number -> number > 5)
+                .skip(1).limit(2).forEach(System.out::println);
 
         //  Print the first number in intNumbersStream that's greater than 5.
         //  If nothing is found, print -1
         // TODO: Write code here
+        final Integer value = StreamSources.intNumbersStream().filter(number -> number > 5)
+                .findFirst().orElse(-1);
+        System.out.println(value);
 
         // Print first names of all users in userStream
         // TODO: Write code here
 
         // Print first names in userStream for users that have IDs from number stream
         // TODO: Write code here
+        StreamSources.intNumbersStream().flatMap(id -> StreamSources.userStream().filter(user -> user.getId() == id))
+                .map(user -> user.getFirstName())
+                .forEach(System.out::println);
+
+        StreamSources.userStream().filter(u -> StreamSources.intNumbersStream()
+                .anyMatch(i -> i == u.getId())).forEach(System.out::println);
+
 
     }
 
--- src/main/java/io/javabrains/reactiveworkshop/Exercise4.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise4.java
@@ -1,6 +1,7 @@
 package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class Exercise4 {
 
@@ -10,10 +11,16 @@ public class Exercise4 {
 
         // Print the value from intNumberMono when it emits
         // TODO: Write code here
+        ReactiveSources.intNumberMono().subscribe(number -> System.out.println(number),
+                error -> System.out.println(error.getMessage()),
+                () -> System.out.println("complete"));
 
         // Get the value from the Mono into an integer variable
         // TODO: Write code here
+//        final Integer number = ReactiveSources.intNumberMono().block();
+        final Optional<Integer> number = ReactiveSources.intNumberMono().blockOptional();
 
+        final User foo = ReactiveSources.userMono().block();
         System.out.println("Press a key to end");
         System.in.read();
     }
